3017258c7e96e56adceaf97400d41ceda6336eea
maciekMerkury <maciekmerkury2@gmail.com>
2021-11-04 15:24:53 +0100

    bruh

--- binance-public-data/shell/download-klines.sh
+++ binance-public-data/shell/download-klines.sh
@@ -3,7 +3,8 @@
 # This is a simple script to download klines by given parameters.
 
 symbols=("XMRUSDT") # add symbols here to download
-intervals=("1m" "5m" "15m" "30m" "1h" "2h" "4h" "6h" "8h" "12h" "1d" "3d" "1w" "1mo")
+#intervals=("1m" "5m" "15m" "30m" "1h" "2h" "4h" "6h" "8h" "12h" "1d" "3d" "1w" "1mo")
+intervals=("1m")
 years=("2017" "2018" "2019" "2020")
 months=(01 02 03 04 05 06 07 08 09 10 11 12)
 
